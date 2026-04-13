03ef6f8acdb59e6b629a30db136f56595e0c5019
Pasandul Wickramaarachchi <thevinuomisha@gmail.com>
2026-04-13 21:16:45 +0530

    chore: remove unused file and associated references

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import App from './App.jsx'
@@ -8,8 +8,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter
-      basename="/estate-agent-app"
+    <HashRouter
       future={{
         v7_startTransition: true,
         v7_relativeSplatPath: true
@@ -18,6 +17,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <DndProvider backend={HTML5Backend}>
         <App />
       </DndProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
